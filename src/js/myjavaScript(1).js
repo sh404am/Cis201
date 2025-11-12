1f0e78809c5c14cc601ee4e88c0e75edb83b1e85
sh404am <skasaballi4@gmail.com>
2025-11-12 23:19:28 +0300

    Create myjavaScript(1).js

---
+++ src/js/myjavaScript(1).js
@@ -0,0 +1,12 @@
+function addP(){
+    var x=document.getElementById("result").value;
+    var y=parseInt(x)+1;
+    document.getElementById("result").value=y;
+
+}
+function deleteP(){
+    var x=document.getElementById("result").value;
+    var y=parseInt(x)-1;
+    document.getElementById("result").value=y;
+
+}
